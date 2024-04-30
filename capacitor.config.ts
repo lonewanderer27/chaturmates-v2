eab3ecf93b9066fbbd561b4b3730f2901e605d53
lonewanderer27 <adrianejames27@gmail.com>
2024-04-30 16:25:51 +0800

    feat(chore): rename project to chaturmates2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.lonewanderer27.chaturmates2',
   appName: 'Chaturmates v2',
   webDir: 'dist'
 };

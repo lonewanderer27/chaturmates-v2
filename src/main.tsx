6d6f5ced928d07fa4297981831db7181f954afcf
lonewanderer27 <adrianejames27@gmail.com>
2024-05-04 11:00:39 +0800

    fix tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import "./theme/variables.css";
+import "./theme/tailwind.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container!);

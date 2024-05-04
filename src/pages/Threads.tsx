ed68cb394683478b59c42d29cd1570a188db5c94
lonewanderer27 <adrianejames27@gmail.com>
2024-05-04 11:16:11 +0800

    basic threads page

--- src/pages/Threads.tsx
+++ src/pages/Threads.tsx
@@ -1,5 +1,20 @@
+import { IonPage, IonContent, IonHeader, IonToolbar, IonText, IonButtons, IonTitle } from "@ionic/react";
+import { searchOutline, notificationsOutline } from "ionicons/icons";
+import NavBtn from "../components/NavBtn";
+
 export default function Threads() {
   return (
-    <div>Threads</div>
+    <IonPage>
+      <IonContent fullscreen className='ion-padding'>
+        <IonHeader collapse='condense'>
+          <IonToolbar>
+            <IonTitle size="large">
+              Threads
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+      </IonContent>
+    </IonPage>
   )
 }

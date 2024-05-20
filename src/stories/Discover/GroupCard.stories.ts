97b895bfb64514acfc2717b9e847e59745bdfbed
lonewanderer27 <adrianejames27@gmail.com>
2024-05-20 14:55:06 +0800

    fix GroupCard import error in storybook

--- src/stories/Discover/GroupCard.stories.ts
+++ src/stories/Discover/GroupCard.stories.ts
@@ -1,5 +1,6 @@
-import GroupCard from '../../components/CommunityPage/GroupCard';
 import type { Meta, StoryObj } from '@storybook/react';
+
+import GroupCard from '../../components/GroupCard';
 
 const meta = {
   title: 'Discover/GroupCard',

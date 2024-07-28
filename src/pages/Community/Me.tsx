71619810ae5731d819c29e08ed57ad0fb34b0922
lonewanderer27 <adrianejames27@gmail.com>
2024-07-28 14:15:41 +0800

    ui: use ionic card in me page

--- src/pages/Community/Me.tsx
+++ src/pages/Community/Me.tsx
@@ -1,79 +1,123 @@
-import { IonBackButton, IonButton, IonButtons, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonText, IonToolbar, useIonRouter } from '@ionic/react'
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonPage,
+  IonRow,
+  IonText,
+  IonToolbar,
+  useIonRouter,
+} from "@ionic/react";
 
-import { FC } from 'react'
-import MemberAvatarLarge from '../../components/Me/MemberAvatarLarge'
-import { RouteComponentProps } from 'react-router'
-import useSelfStudent from '../../hooks/student'
-import useSession from '../../hooks/auth/useSession'
+import { FC } from "react";
+import MemberAvatarLarge from "../../components/Me/MemberAvatarLarge";
+import { RouteComponentProps } from "react-router";
+import useSelfStudent from "../../hooks/student";
+import useSession from "../../hooks/auth/useSession";
 
 const Me: FC<RouteComponentProps> = ({ match }) => {
   const { session } = useSession();
   const rt = useIonRouter();
-  const { student, profile, groups, school, academic_year, following, followers } = useSelfStudent();
+  const {
+    student,
+    profile,
+    groups,
+    school,
+    academic_year,
+    following,
+    followers,
+  } = useSelfStudent();
 
   const handleFollowing = () => {
-    rt.push(rt.routeInfo.pathname+"/following")
-  }
+    rt.push(rt.routeInfo.pathname + "/following");
+  };
 
   const handleGroups = () => {
-    rt.push(rt.routeInfo.pathname+"/groups")
-  }
+    rt.push(rt.routeInfo.pathname + "/groups");
+  };
 
   const handleUpdate = () => {
-    rt.push(rt.routeInfo.pathname+"/update")
-  }
+    rt.push(rt.routeInfo.pathname + "/update");
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons>
-            <IonBackButton className='ml-2' defaultHref="/community" text={""} />
+            <IonBackButton
+              className="ml-2"
+              defaultHref="/community"
+              text={""}
+            />
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
+      <IonContent className="ion-padding">
         <MemberAvatarLarge avatarUrl={student?.avatar_url} />
-        <div className="pt-16 bg-slate-200 rounded-xl">
+        <IonCard className="pt-16 mx-0 bg-slate-200  z-[-500] shadow-none">
           <IonCardContent>
-            <div className='flex justify-end mt-[-30px] z-100'>
-              <IonButton size="small" onClick={handleUpdate}>Edit</IonButton>
-            </div>
             <IonGrid>
-              <IonRow className='flex justify-center'>
-                <IonText>{student?.full_name ?? "-"}</IonText>
+              <IonRow className="flex justify-end mt-[-70px] pb-4 z-[1000]">
+                <IonButton size="small" onClick={handleUpdate}>
+                  Edit
+                </IonButton>
               </IonRow>
-              <IonRow className='flex justify-center mt-1'>
-                <IonText className='text-sm capitalize' color="medium">{student?.type ?? "-"}</IonText>
+              <IonRow className="flex justify-center">
+                <IonText color="dark">{student?.full_name ?? "-"}</IonText>
               </IonRow>
-              <IonRow className='text-center mt-2'>
-                <IonCol className='cursor-pointer' onClick={handleFollowing}>
-                  <IonText className='text-2xl'>{following?.length ?? "-"}</IonText>
+              <IonRow className="flex justify-center mt-1">
+                <IonText className="text-sm capitalize" color="dark">
+                  {student?.type ?? "-"}
+                </IonText>
+              </IonRow>
+              <IonRow className="text-center mt-2">
+                <IonCol className="cursor-pointer" onClick={handleFollowing}>
+                  <IonText className="text-2xl" color="dark">
+                    {following?.length ?? "-"}
+                  </IonText>
                   <br />
-                  <IonText className="text-sm">following</IonText>
+                  <IonText className="text-sm" color="dark">
+                    following
+                  </IonText>
                 </IonCol>
-                <IonCol className='cursor-pointer' onClick={handleGroups}>
-                  <IonText className='text-2xl'>{groups?.length ?? "-"}</IonText>
+                <IonCol className="cursor-pointer" onClick={handleGroups}>
+                  <IonText className="text-2xl" color="dark">
+                    {groups?.length ?? "-"}
+                  </IonText>
                   <br />
-                  <IonText className="text-sm">groups</IonText>
+                  <IonText className="text-sm" color="dark">
+                    groups
+                  </IonText>
                 </IonCol>
               </IonRow>
             </IonGrid>
           </IonCardContent>
-        </div>
-        <div className='mt-4 rounded-xl bg-slate-200'>
+        </IonCard>
+        <IonCard className="mt-4 mx-0 rounded-xl bg-slate-200 shadow-none">
           <IonCardContent>
             <div>
-              <IonText className='text-xs font-bold'>BIO</IonText><br />
+              <IonText className="text-xs font-bold" color="dark">
+                BIO
+              </IonText>
+              <br />
             </div>
-            <div className=''>
-              <IonText className='text-sm'>{student?.description}</IonText>
+            <div className="">
+              <IonText className="text-sm" color="dark">
+                {student?.description}
+              </IonText>
             </div>
           </IonCardContent>
-        </div>
+        </IonCard>
       </IonContent>
     </IonPage>
-  )
-}
+  );
+};
 
-export default Me
+export default Me;

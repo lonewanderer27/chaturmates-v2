9d0cffea2d1e1bb45be3d509c7014774c204c7b7
lonewanderer27 <adrianejames27@gmail.com>
2024-07-30 18:06:09 +0800

    feat: following & notice segment in Inbox

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,11 +1,46 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react'
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
+  useIonRouter,
+  useIonViewWillEnter,
+} from "@ionic/react";
+import { showTabBar } from "../utils/TabBar";
+import { FC, useState, useEffect } from "react";
+import { RouteComponentProps } from "react-router";
+import { useLocation } from "react-router-dom";
+import { INBOX_CATEGORY } from "../enums/inbox";
 
-import { showTabBar } from '../utils/TabBar';
+const Inbox: FC<RouteComponentProps> = () => {
+  const location = useLocation();
+  const history = useIonRouter();
+  const [selectedSegment, setSelectedSegment] = useState<INBOX_CATEGORY>(
+    INBOX_CATEGORY.FOLLOWING
+  );
 
-export default function Inbox() {
   useIonViewWillEnter(() => {
     showTabBar();
-  })
+  });
+
+  useEffect(() => {
+    if (location.hash === "#notice") {
+      setSelectedSegment(INBOX_CATEGORY.NOTICE);
+    } else {
+      setSelectedSegment(INBOX_CATEGORY.FOLLOWING);
+    }
+  }, [location.hash]);
+
+  const handleSegmentChange = (e: CustomEvent) => {
+    const value = e.detail.value as INBOX_CATEGORY;
+    setSelectedSegment(value);
+    history.push(`/inbox#${value === INBOX_CATEGORY.NOTICE ? "notice" : "following"}`);
+  };
 
   return (
     <IonPage>
@@ -13,20 +48,25 @@ export default function Inbox() {
         <IonToolbar>
           <IonTitle>Inbox</IonTitle>
           <IonButtons slot="start">
-            <IonBackButton defaultHref='/discover' />
+            <IonBackButton defaultHref="/discover" />
           </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen className='ion-padding'>
-        <IonSegment>
-          <IonSegmentButton>
+      <IonContent fullscreen className="ion-padding">
+        <IonSegment
+          value={selectedSegment}
+          onIonChange={handleSegmentChange}
+        >
+          <IonSegmentButton value={INBOX_CATEGORY.FOLLOWING}>
             Following
           </IonSegmentButton>
-          <IonSegmentButton>
-            Notices
+          <IonSegmentButton value={INBOX_CATEGORY.NOTICE}>
+            Notice
           </IonSegmentButton>
         </IonSegment>
       </IonContent>
     </IonPage>
-  )
-}
+  );
+};
+
+export default Inbox;

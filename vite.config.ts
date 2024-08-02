b161e4bef9297a31160ca6f728fcca81294d8ffb
lonewanderer27 <adrianejames27@gmail.com>
2024-08-03 03:17:33 +0800

    perf: remove console logs on prod build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), legacy()],
+    esbuild: {
+      pure:
+        mode === "production"
+          ? ["console.log", "console.info", "console.error"]
+          : [],
+    },
+    // test: {
+    //   globals: true,
+    //   environment: 'jsdom',
+    //   setupFiles: './src/setupTests.ts',
+    // }
+  };
+});

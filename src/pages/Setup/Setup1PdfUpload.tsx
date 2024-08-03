a1bd7e466918666907d510abd38e9de7609372bb
lonewanderer27 <adrianejames27@gmail.com>
2024-08-03 08:34:47 +0800

    perf: use a faster langPath for tesseractjs

--- src/pages/Setup/Setup1PdfUpload.tsx
+++ src/pages/Setup/Setup1PdfUpload.tsx
@@ -109,6 +109,7 @@ const SetupPdfUpload: FC<RouteComponentProps> = ({ match }) => {
 
   const worker = createWorker({
     logger: (m) => console.log(m),
+    langPath: "https://tessdata.projectnaptha.com/4.0.0_fast",
   });
   const [loader, dismissLoader] = useIonLoading();
   const { data: subjects } = useFetchSubjects();

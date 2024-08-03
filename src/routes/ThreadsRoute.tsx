02a8a357789d16cc27284aa0de0df390abfba6be
lonewanderer27 <adrianejames27@gmail.com>
2024-08-03 19:27:36 +0800

    hide threads page behind feature unavailable

--- src/routes/ThreadsRoute.tsx
+++ src/routes/ThreadsRoute.tsx
@@ -2,13 +2,14 @@ import { IonRouterOutlet } from '@ionic/react'
 import React from 'react'
 import Threads from '../pages/Threads'
 import { Route, RouteComponentProps } from 'react-router'
+import FeatureUnavailable from './FeatureUnavailable'
 
 const ThreadsRoute: React.FC<RouteComponentProps> = ({ match }) => {
   console.log(match);
 
   return (
     <IonRouterOutlet id="threads">
-      <Route path={match.url} component={Threads} exact />
+      <Route path={match.url} component={FeatureUnavailable} exact />
     </IonRouterOutlet>
   )
 }

311db6cb4cbfc96a2eddc06999686440a4bbb2df
lonewanderer27 <adrianejames27@gmail.com>
2024-08-05 21:43:22 +0800

    perf: lazy load Setup & Surveys route

--- src/routes/SetupRoute.tsx
+++ src/routes/SetupRoute.tsx
@@ -1,26 +1,28 @@
-import { IonRouterOutlet } from '@ionic/react'
-
-import React from 'react'
+import { IonLoading, IonRouterOutlet } from '@ionic/react'
+import React, { lazy, Suspense } from 'react'
 import { Route } from 'react-router'
 import { RouteComponentProps } from 'react-router'
-import Setup from '../pages/Setup'
-import SetupPdfUpload from '../pages/Setup/Setup1PdfUpload'
-import Setup3AcademicInformation from '../pages/Setup/Setup3AcademicInformation'
-import Setup4Subjects from '../pages/Setup/Setup4Subjects'
-import Setup6Finish from '../pages/Setup/Setup6Finish'
-import SetupStudentOrProf from '../pages/Setup/Setup0StudentOrProf'
-import Setup2IntroduceYourself from '../pages/Setup/Setup2IntroduceYourself'
+
+const Setup = lazy(() => import('../pages/Setup'))
+const SetupPdfUpload = lazy(() => import('../pages/Setup/Setup1PdfUpload'))
+const Setup3AcademicInformation = lazy(() => import('../pages/Setup/Setup3AcademicInformation'))
+const Setup4Subjects = lazy(() => import('../pages/Setup/Setup4Subjects'))
+const Setup6Finish = lazy(() => import('../pages/Setup/Setup6Finish'))
+const SetupStudentOrProf = lazy(() => import('../pages/Setup/Setup0StudentOrProf'))
+const Setup2IntroduceYourself = lazy(() => import('../pages/Setup/Setup2IntroduceYourself'))
 
 const SetupRoute: React.FC<RouteComponentProps> = ({ match }) => {
   return (
     <IonRouterOutlet id="setup">
-      <Route path={match.url} component={Setup} exact />
-      <Route path={`${match.url}/studentOrProf`} component={SetupStudentOrProf} exact />
-      <Route path={`${match.url}/pdfUpload`} component={SetupPdfUpload} exact />
-      <Route path={`${match.url}/introduceYourself`} component={Setup2IntroduceYourself} exact />
-      <Route path={`${match.url}/academicInformation`} component={Setup3AcademicInformation} exact />
-      <Route path={`${match.url}/subjects`} component={Setup4Subjects} exact />
-      <Route path={`${match.url}/finish`} component={Setup6Finish} exact />
+      <Suspense fallback={<IonLoading message={"Setup is loading"} />}>
+        <Route path={match.url} component={Setup} exact />
+        <Route path={`${match.url}/studentOrProf`} component={SetupStudentOrProf} exact />
+        <Route path={`${match.url}/pdfUpload`} component={SetupPdfUpload} exact />
+        <Route path={`${match.url}/introduceYourself`} component={Setup2IntroduceYourself} exact />
+        <Route path={`${match.url}/academicInformation`} component={Setup3AcademicInformation} exact />
+        <Route path={`${match.url}/subjects`} component={Setup4Subjects} exact />
+        <Route path={`${match.url}/finish`} component={Setup6Finish} exact />
+      </Suspense>
     </IonRouterOutlet>
   )
 }
--- src/routes/SurveysRoute.tsx
+++ src/routes/SurveysRoute.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { Route, RouteComponentProps } from 'react-router';
 import { hideTabBar, showTabBar } from '../utils/TabBar';
 
-import EnrollmentCert from '../pages/Surveys/EnrollmentCert';
-import { IonRouterOutlet } from '@ionic/react';
+import { IonLoading, IonRouterOutlet } from '@ionic/react';
 import useHideTabs from '../hooks/useHideTabs';
+
+const EnrollmentCert = React.lazy(() => import('../pages/Surveys/EnrollmentCert'));
 
 const SurveysRoute: React.FC<RouteComponentProps> = ({ match }) => {
   useHideTabs();
 
   return (
     <IonRouterOutlet id="surveys">
-      <Route path={`${match.url}/enrollmentcert`} component={EnrollmentCert} exact />
+      <React.Suspense fallback={<IonLoading message="Survey is loading" />}>
+        <Route path={`${match.url}/enrollmentcert`} component={EnrollmentCert} exact />
+      </React.Suspense>
     </IonRouterOutlet>
-  )
-}
+  );
+};
 
-export default SurveysRoute
+export default SurveysRoute;

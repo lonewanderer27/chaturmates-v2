3601541842660483d845a3fceb3ea72670f247cb
lonewanderer27 <adrianejames27@gmail.com>
2024-09-01 23:25:01 +0800

    ui(AddGroupCard): flush

--- src/components/DiscoverPage/AddGroupCard.tsx
+++ src/components/DiscoverPage/AddGroupCard.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonCol, IonIcon, IonLabel, useIonRouter } from "@ionic/react";
+import { IonCard, IonCol, IonIcon, IonLabel, IonText, useIonRouter } from "@ionic/react";
 
 import { addCircleOutline } from "ionicons/icons";
 
@@ -10,13 +10,13 @@ export default function AddGroupCard() {
 
   return (
     <IonCol size="6" className="flex flex-column w-full cursor-pointer">
-      <IonCard
-        className="groupCard ion-padding ion-no-margin w-full flex flex-col  align-items-center justify-center text-center"
+      <div
+        className=" ion-padding ion-no-margin w-full flex flex-col  align-items-center justify-center text-center"
         onClick={handleClick}
       >
-        <IonIcon className="text-5xl mx-auto" src={addCircleOutline}></IonIcon>
-        <IonLabel className="my-2 text-lg font-poppins font-medium">New Group</IonLabel>
-      </IonCard>
+        <IonIcon className="text-5xl mx-auto" src={addCircleOutline} color="medium"></IonIcon>
+        <IonLabel className="my-2 text-lg font-poppins font-semibold" color="medium">New Group</IonLabel>
+      </div>
     </IonCol>
   );
 }
